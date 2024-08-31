29d6cb3cb4c4fc3609614f6f302e9f08e54fcf0d
Abid Ali Awan <abidaliawan@rocketmail.com>
2024-09-01 01:00:53 +0500

    removing some features

--- app.py
+++ app.py
@@ -74,13 +74,9 @@ demo = gr.Interface(
     inputs="text",
     outputs="text",
     live=True,
-    batch=True,
-    max_batch_size=10000,
-    concurrency_limit=16,
     allow_flagging="never",
     theme=gr.themes.Soft(),
 )
 
 # Launch the Gradio interface
-demo.queue()
 demo.launch()
